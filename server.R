339dad87b1cc689dc2d51a4ca3133fdc30c81fd7
nsfphd <noellesfoster@me.com>
2018-01-16 10:03:02 -0500

    Final files

--- server.R
+++ server.R
@@ -3,8 +3,10 @@ library(HistData)
 data("Galton") 
 
 shinyServer(function(input, output) {
-  src_data<-Galton
-  
+    parent_model<-lm(child~parent, data=Galton)
+    child_model<-lm(parent~child, data=Galton)
+    model<-parent_model
+    
     set_short_independent<-reactive({
       value<-input$data
       return(value)
@@ -15,27 +17,31 @@ shinyServer(function(input, output) {
            "child"=Galton$child,
            "parent"=Galton$parent)
     })
+    
     output$summary<-renderPrint({
       data<-set_long_independent()
-    summary(data)
+      summary(data)
     })
+    
     output$hist_plot<-renderPlot({
-      x<-set_long_independent()
-      bins<-seq(min(x), max(x), length.out=input$bins+1)
-      hist(x, breaks=bins, col="blue", border="white",
-           xlab=paste("Histogram of", set_short_independent(), "dataset"))
+      dataset<-set_long_independent()
+      bins<-seq(min(dataset), max(dataset), length.out=input$bins+1)
+      hist(dataset, breaks=bins, col="blue",xlab=paste("Histogram of", 
+           set_short_independent(), "dataset"))
     })
+    
     set_short_dependent<-reactive({
-      if(set_short_independent()==child){
-        dependent<-parent
+      if(set_short_independent()=="child"){
+        dependent<-"parent"
       }
       else{
-        dependent<-child
+        dependent<-"child"
       }
       return(dependent)
     })
+    
     set_long_dependent<-reactive({
-      if(set_long_independent()==Galton$child){
+      if (set_short_independent()=="child"){
         dependent<-Galton$parent
       }
       else{
@@ -43,16 +49,37 @@ shinyServer(function(input, output) {
       }
       return(dependent)
     })
-  model<-lm(child~parent, data=Galton)  
- 
+
+      set_model<- reactive({
+          if(set_short_independent()=="child"){
+            model<-child_model
+          }
+        return(model)
+        })
+    
+      set_pred_model<-reactive({
+        hgt_input <- input$pred
+        if(set_short_independent()=="child"){
+          predict(set_model(), newdata = data.frame(child=hgt_input))
+        }
+        else{
+          predict(set_model(), newdata=data.frame(parent=hgt_input))
+        }
+      })
+      
     output$pred_plot <- renderPlot({
+      hgt_input <- input$pred
+      plot(set_long_dependent(), set_long_independent(), xlab = paste("Outcome: ", set_short_dependent(),"'s height"), 
+           ylab = paste("Predictor: ", set_short_independent(),"'s height"), bty = "n", pch = 16,
+           xlim = c(60, 80), ylim = c(60, 80))
+      abline(set_model(), col = "red", lwd = 2)
+      points(hgt_input, set_pred_model(), col = "red", pch = 16, cex = 2)
+    })
+    
+    output$prediction<-renderText({
+      paste("Forecasted outcome from selected height:  ", set_pred_model())
+    })
+    
 
       
-      plot(Galton$child, Galton$parent, xlab = "Child's height", 
-           ylab = "Parent's height", bty = "n", pch = 16,
-           xlim = c(50, 80), ylim = c(50, 80))
-      abline(model, col = "red", lwd = 2)
-      })
-    
-   
-})
+    })
--- ui.R
+++ ui.R
@@ -10,23 +10,32 @@ shinyUI(fluidPage(
       tags$div(class="header", checked=NA,
                tags$p("This application will provide basic information about 
                       the distribution and summary statistics for the Galton 
-                      paired parent/child height datasets."),
+                      paired parent/child height datasets, along with a graph
+                      representing the relationship between the two and predicted 
+                      outcome heights based on input."),
                tags$br()
                ),
-      radioButtons("data", "Choose parent or child data for histogram:",
+      radioButtons("data", "Choose parent or child data for graphing and prediction:",
                    c("Parent" = "parent",
                      "Child" = "child")),
       br(),
       
        sliderInput("bins",
                    "Number of bins:",
-                   min = 10,
-                   max = 50,
-                   value = 20)
+                   min = 8,
+                   max = 20,
+                   value = 10),
+      numericInput("pred", "Height (in inches) to predict from:", value=60, 
+                   min=60, max=75, step=0.1)
     ),
     mainPanel(
-      plotOutput("hist_plot"),
-      verbatimTextOutput("summary"))
+      tabsetPanel(
+        tabPanel("Summary data and histogram for selected dataset",
+                 plotOutput("hist_plot"),
+                 verbatimTextOutput("summary")),
+        tabPanel("Predictions from selected dataset",
+                 plotOutput("pred_plot"),
+                 verbatimTextOutput("prediction")))
     )
 )
-)
+))
